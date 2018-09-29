6a4eafe76cad30302ff7e6be28382fb38c0cc275
Leifur Sigurdsson <leifurerik@gmail.com>
2018-09-29 18:47:00 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,3 +5,23 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
+def won?(WIN_COMBINATIONS)
+  WIN_COMBINATIONS.each do |check|
+    win_index_1 = check[0]
+    win_index_2 = check[1]
+    win_index_3 = check[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combination
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combination
+    else
+      return false
+    end
+  end
+end
